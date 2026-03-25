e5c6633754144cd0cfe60e209054cbfe3e0f517b
Csongor123 <csongi054@gmail.com>
2026-03-25 18:57:21 +0100

    Lexikális elemző

--- Lexikális/main.cpp
+++ Lexikális/main.cpp
@@ -1,118 +1,115 @@
-#include <stdio.h>
-#include <string.h>
-#pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
 
 // Globalis valtozok
-static char szamjegyek[] = "0123456789";
-static char betuk[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string szamjegyek = "0123456789";
+string betuk = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// Fuggveny, ami ellenorzi, hogy egy adott karakter szerepel-e egy karakterlancban
-int StrContains(char value, char str[]) {
-	for (size_t i = 0; i < strlen(str); i++) {
-		if (str[i] == value) {
-			return 1;
-		}
-	}
-	return 0;
+// Ellenorzi, hogy egy karakter szerepel-e egy sztringben
+bool StrContains(char value, const string& str) {
+    for (size_t i = 0; i < str.length(); i++) {
+        if (str[i] == value) {
+            return true;
+        }
+    }
+    return false;
 }
-// 0: azon, 1: konst, 2:kommentA, 3:kommentB,-1:none
+
+// 0: azonosito, 1: konstans, 2: komment (* *), 3: komment { }, -1: nincs
 int ModeCheck(char c, char prevC, int m) {
-	if (StrContains(c, betuk) && m == -1) {
-		return 0;
-	}
-	if ((StrContains(c, szamjegyek) && m == -1)) {
-		return 1;
-	}
-	if ((prevC== '(' && c == '*') && m == -1) {
-		return 2;
-	}
-	if (c == '{' && m == -1) {
-		return 3;
-	}
-	return m;
+    if (StrContains(c, betuk) && m == -1) {
+        return 0;
+    }
+    if (StrContains(c, szamjegyek) && m == -1) {
+        return 1;
+    }
+    if (prevC == '(' && c == '*' && m == -1) {
+        return 2;
+    }
+    if (c == '{' && m == -1) {
+        return 3;
+    }
+    return m;
 }
 
-int LexikalisElemzes(char utasitas[]) {
-	printf("utasitas: %s\n", utasitas);
+void LexikalisElemzes(const string& utasitas) {
+    cout << "utasitas: " << utasitas << endl;
 
-	char prevChar = '-';
+    char prevChar = '-';
+    string token = "";
+    int mode = -1;
 
-	char token[100] = "";
-	int mode = -1;
-	for (int i = 0; i <= strlen(utasitas); i++) {
-		mode = ModeCheck(utasitas[i], prevChar, mode);
+    for (size_t i = 0; i < utasitas.length(); i++) {
+        mode = ModeCheck(utasitas[i], prevChar, mode);
 
-		// Azonosito ellenorzes
-		if (mode == 0) {
-			if (!(StrContains(utasitas[i], betuk)) && !(StrContains(utasitas[i], szamjegyek))) {
-				printf("%s = <azonosito>\n", token);
-				mode = ModeCheck(utasitas[i], prevChar, -1);
-				strcpy(token, "");
-			}
-		}
-		// Konstans ellenorzes
-		if (mode == 1) {
-			if (!(StrContains(utasitas[i], szamjegyek))) {
-				printf("%s = <konstans>\n", token);
-				mode = ModeCheck(utasitas[i], prevChar, -1);
-				strcpy(token, "");
-			}
-		}
-		strncat(token, &utasitas[i], 1);
+        // Azonosito
+        if (mode == 0) {
+            if (!StrContains(utasitas[i], betuk)
+                && !StrContains(utasitas[i], szamjegyek)) {
+                cout << token << " = <azonosito>" << endl;
+                token = "";
+                mode = ModeCheck(utasitas[i], prevChar, -1);
+            }
+        }
 
-		// (*Komment*) ellenorzes
-		if (mode == 2) {
-			if (prevChar == '*' && utasitas[i] == ')') {
-				//printf(" = <komment>\n");
-				mode = ModeCheck(utasitas[i], prevChar, -1);
-				strcpy(token, "");
-			}
-		}
-		// {Komment} ellenorzes
-		if (mode == 3) {
-			if (utasitas[i] == '}') {
-				//printf(" = <komment>\n");
-				mode = ModeCheck(utasitas[i], prevChar, -1);
-				strcpy(token, "");
-			}
-		}
+        // Konstans
+        if (mode == 1) {
+            if (!StrContains(utasitas[i], szamjegyek)) {
+                cout << token << " = <elojel_nelkuli_egesz>" << endl;
+                token = "";
+                mode = ModeCheck(utasitas[i], prevChar, -1);
+            }
+        }
 
-		// Ertekadas ellenorzes
-		if (prevChar == ':' && utasitas[i] == '=') {
-			printf("%s = <ertekadas>\n", token);
-			strcpy(token, "");
-		}
+        token += utasitas[i];
 
-		// Relacio ellenorzes
-		if ((prevChar == '<' && utasitas[i] == '=') ||
-			(prevChar == '>' && utasitas[i] == '=') ||
-			(prevChar == '<' && utasitas[i] == '>')) {
-			printf("%s = <relacio>\n", token);
-			strcpy(token, "");
-		}
+        // (* komment *)
+        if (mode == 2) {
+            if (prevChar == '*' && utasitas[i] == ')') {
+                token = "";
+                mode = -1;
+            }
+        }
 
-		// EoF ellenorzes
-		if (utasitas[i] == '$') {
-			//printf(" = <End of File>\n\n");
-			printf("\n");
-			mode = -1;
-			return 0;
-		}
-		prevChar = utasitas[i];
-	}
+        // { komment }
+        if (mode == 3) {
+            if (utasitas[i] == '}') {
+                token = "";
+                mode = -1;
+            }
+        }
 
-	return 0;
+        // Ertekadas
+        if (prevChar == ':' && utasitas[i] == '=') {
+            cout << token << " = <ertekadas>" << endl;
+            token = "";
+        }
+
+        // Relaciok
+        if ((prevChar == '<' && utasitas[i] == '=')
+            || (prevChar == '<' && utasitas[i] == '>')
+            || (prevChar == '>' && utasitas[i] == '=')) {
+            cout << token << " = <relacio>" << endl;
+            token = "";
+        }
+
+        // EOF
+        if (utasitas[i] == '$') {
+            cout << "$ = <EOF>" << endl << endl;
+            return;
+        }
+
+        prevChar = utasitas[i];
+    }
 }
 
 int main() {
-	// LexikalisElemzes fuggveny tesztelese
-	char utasitasA[] = "kiss23:=12>=nagy11144<>11144(*ize*)$";
-	char utasitasB[] = "valami5uperAzon12:=(*{komment kommentbe?}*)684{erdekes komment}<>123>=(*valami*)unalmasAzon$";
-	char utasitasC[] = "eofTest231:=0{234}$<>azon123<=42135";
+    string utasitasA = "Nagy21<=10<>11:=Kiss22{Nagy23}(:aa=bb:)$";
 
-	LexikalisElemzes(utasitasA);
-	LexikalisElemzes(utasitasB);
-	LexikalisElemzes(utasitasC);
 
-	return 0;
+    LexikalisElemzes(utasitasA);
+
+
+    return 0;
 }
